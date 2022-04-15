c77ff26ca7a2651835f91aad04ea821d6e9b860e
Felipe Pupo <felipepupo@outlook.com.br>
2022-04-15 16:25:16 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,27 +1,6 @@
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-
 const app = express();
 
-app.get('/akela', (request, response) => response.json({ message: 'Hello' }));
 app.use('/', express.static('src/public'));
 
-const port = 6969;
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) =>
-  ws.on('message', (data) => {
-    const message = data.toString();
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  })
-);
-
-server.listen(port, () =>
-  console.log(`Server is listening on http://localhost:${port}!`)
-);
+app.listen(process.env.PORT || 8080)
